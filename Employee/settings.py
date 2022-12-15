20fc7a4418d62a2a7cbc27052571b82d0bd59e08
Aradhyansasi <aradhya.n.sasi101@gmail.com>
2022-12-15 16:34:09 +0530

    updated

--- Employee/settings.py
+++ Employee/settings.py
@@ -125,8 +125,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = "587"
-EMAIL_HOST_USER = "aradhya.n.sasi101@gmail.com"
-EMAIL_HOST_PASSWORD = "hrmjitcsnipsjlzx"
+EMAIL_HOST_USER = "mailsample359@gmail.com"
+EMAIL_HOST_PASSWORD = "mzkkfertokgliscx"
 EMAIL_USE_TLS = True
 # EMAIL_BACKEND = 'django.core.mail'
 
